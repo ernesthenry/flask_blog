0b4cf2f8abeb7e54b47f56d0a08a8825e4b85e3e
ernesthenry <henry38ernest@gmail.com>
2023-07-22 16:10:17 +0300

    Fix error on create endpoint

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from .models import app, setup_db, db_drop_and_create_all
 from .models import Users, Posts
 
 """Endpoint to fetch all blogs or posts"""
-@app.route('/posts', methods=['GET'])
+@app.route('/api/v1/posts', methods=['GET'])
 def current_blogs():
     blogs = Posts.query.all()
     formatted_blogs = [blog.format_record() for blog in blogs]
@@ -16,7 +16,7 @@ def current_blogs():
         }),200
     
 """ Endpoint to fetch single post or  blog """
-@app.route('/post/<int:post_id>',methods=['GET'])
+@app.route('/api/v1/post/<int:post_id>',methods=['GET'])
 def fetch_blog(post_id):
     post = Posts.query.filter(Posts.id == post_id).first()
 
@@ -31,20 +31,19 @@ def fetch_blog(post_id):
 
 
 """Endpoint to create a new blog or post"""
-@app.route('/new-post', methods=['POST'])
+@app.route('/api/v1/new-post', methods=['POST'])
 def add_blog():
     body = request.get_json()
     title = body.get('title', None)
     description = body.get('description', None)
     try:
-        title = Posts(title==title)
-        print(title)
-        description = Posts(description==description)
-        print(description)
+         # Check if both 'title' and 'description' are provided in the request
+        if not title or not description:
+            return {'message': 'Title and description are required fields.', 'error_code': '422'}, 422
 
         new_blog = Posts(title=title, description=description)
 
-        # add the new blog to the database
+        # Add the new blog to the database
         new_blog.insert()
         print("New Blog has been created successfully")
 
@@ -56,8 +55,6 @@ def add_blog():
             })
     except:
         abort(422)                      
-
-
 
 def create_app(app,test_test_config=None):
     app.config['SECRET_KEY']='57324676734hjvbedhjewr9pp942312y89r321g8t7'
